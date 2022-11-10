ef5e2c9852aca9103ddbf05fb05cc2824ee4f046
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-11-10 17:28:03 +0300

    display all books

--- resources/views/components/book/card.blade.php
+++ resources/views/components/book/card.blade.php
@@ -1,8 +1,20 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-    {{-- <p>{{ $book->title }}</p>
-    <p>{{ $book->description }}</p>
-    <p>{{ \Carbon\Carbon::parse($book->created_at)->diffForHumans() }}</p> --}}
+<div id="card">
+    <div>
+        <div>
+            <img src={{ asset('storage/madalyn-cox-O7ygzpAL4Mc-unsplash.jpg') }} class="card-header">
+        </div>
+        <div class="card-body">
+            <p class="difference">{{ \Carbon\Carbon::parse($book->created_at)->diffForHumans() }}</p>
+            <div>
+                <p class="title">{{ $book->title }}</p>
+            </div>
+            <div>
+                <p class="description">
+                    {{ $book->description }}
+                </p>
+            </div>
+        </div>
+    </div>
     <div class="grid-container card-footer">
         <div class="grid-container">
             <p>24K downloads</p>
@@ -15,34 +27,3 @@
         <p>William Shakespeare</p>
     </div>
 </div>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Jost:wght@300;400&display=swap');
-    .card-footer {
-        margin-left: 25px;
-        position: absolute;
-        width: 250px;
-        height: 60px;
-        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-        background: linear-gradient(180deg, #D98B60 13.02%, #D5395E 94.27%);
-        border-radius: 4px 4px 20px 20px;
-    }
-    p {
-        margin-top: 2px;
-        font-family: 'Jost';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 15px;
-        text-align: center;
-        color: #FFFFFF;
-    }
-    .line {
-        width: 0;
-        border: 1px solid #797373;
-        opacity: 0.5;
-    }
-    .grid-container {
-        display: grid;
-        grid-template-columns: auto auto auto;
-    }
-</style>
--- resources/views/pages/book/list.blade.php
+++ resources/views/pages/book/list.blade.php
@@ -9,9 +9,89 @@
     </head>
     
     <body>
-        <x-book.card />
-        {{-- @foreach ($books as $book)
-            <x-book.card :book="$book" />
-        @endforeach     --}}
+        <div class="grid-container">
+            @foreach ($books as $book)
+                <div>
+                    <x-book.card :book="$book" />
+                </div>
+            @endforeach
+        </div>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Jost:wght@300;400&display=swap');
+        
+            .card-header {
+                width: 250px;
+                height: 150px;
+                border-radius: 25px 25px 0px 0px;
+            }
+            .card-body {
+                width: 250px;
+                height: 130px;
+                background: rgba(245, 245, 245, 0.15);
+                border-radius: 1px;
+                top: 148px;
+                margin-top: 0px;
+            }
+            .card-footer {
+                top: 278px;
+                
+                width: 250px;
+                height: 55px;
+                filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+                background: linear-gradient(180deg, #D98B60 13.02%, #D5395E 94.27%);
+                border-radius: 4px 4px 20px 20px;
+            }
+            p {
+                margin-top: 2px;
+                font-family: 'Jost';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 15px;
+                text-align: center;
+                color: #FFFFFF;
+            }
+            .line {
+                width: 0;
+                border: 1px solid #797373;
+                opacity: 0.5;
+            }
+            .difference {
+                font-family: 'Jost';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 16px;
+                text-align: center;
+                color: #E07057;
+            }
+            .grid-container {
+                display: inline-grid;
+                grid-template-columns: auto auto auto;
+            }
+            .title {
+                margin-top: 10px;
+                font-family: 'Jost';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 22px;
+                line-height: 15px;
+                text-align: center;
+                color: #000000c4;
+            }
+            .description {
+                font-family: 'Jost';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 13px;
+                margin-top: 8px;
+                line-height: 18px;
+                text-align: center;
+                color: rgba(0, 0, 0, 0.49);
+            }
+            #card {
+                margin: 10px;
+                position: relative;
+                filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+            }
+        </style>
     </body>
 </html>
